3f4ad6e9fd0004f3b00b742154705bb9fd07fba8
NancyBolstad <litchacha@gmail.com>
2020-03-12 17:30:10 +0100

    Updates

---
+++ precache-manifest.328f265cb22d5cc34ea1fe9766f14c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e417e2745f5723364df50b37947ef7d",
+    "url": "/noroff-react-ma3/index.html"
+  },
+  {
+    "revision": "82e465bca1bcb8a88d86",
+    "url": "/noroff-react-ma3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e424af51329878431237",
+    "url": "/noroff-react-ma3/static/js/2.f1d57b15.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/noroff-react-ma3/static/js/2.f1d57b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e465bca1bcb8a88d86",
+    "url": "/noroff-react-ma3/static/js/main.f4ae2b09.chunk.js"
+  },
+  {
+    "revision": "add0e389514841524ed6",
+    "url": "/noroff-react-ma3/static/js/runtime-main.7382423a.js"
+  }
+]);
