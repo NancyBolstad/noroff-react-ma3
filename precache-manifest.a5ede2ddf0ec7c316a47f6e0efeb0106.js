95f1c10eec4fa4ec403c8b606fff1b59ac02b347
NancyBolstad <litchacha@gmail.com>
2020-03-12 22:08:07 +0100

    Updates

--- precache-manifest.a5ede2ddf0ec7c316a47f6e0efeb0106.js
+++ precache-manifest.a5ede2ddf0ec7c316a47f6e0efeb0106.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd613b48d2487fcc5e54faabc0a5b9a4",
+    "revision": "da18026093e2950f56e823d253db6ec8",
     "url": "/noroff-react-ma3/index.html"
   },
   {
-    "revision": "9cf5b623793c6e43653d",
+    "revision": "73bc6ae695625670ab22",
     "url": "/noroff-react-ma3/static/css/main.5facb584.chunk.css"
   },
   {
-    "revision": "16a9a9cd7b43e348019e",
-    "url": "/noroff-react-ma3/static/js/2.d227853c.chunk.js"
+    "revision": "aca391a515031a9387b0",
+    "url": "/noroff-react-ma3/static/js/2.3e983f1f.chunk.js"
   },
   {
     "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
-    "url": "/noroff-react-ma3/static/js/2.d227853c.chunk.js.LICENSE.txt"
+    "url": "/noroff-react-ma3/static/js/2.3e983f1f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9cf5b623793c6e43653d",
-    "url": "/noroff-react-ma3/static/js/main.ffdfa73c.chunk.js"
+    "revision": "73bc6ae695625670ab22",
+    "url": "/noroff-react-ma3/static/js/main.1a00dc20.chunk.js"
   },
   {
     "revision": "add0e389514841524ed6",
