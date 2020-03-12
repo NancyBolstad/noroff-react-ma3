6abaa14b868ab9152310fa2161e5e521e8e31b01
NancyBolstad <litchacha@gmail.com>
2020-03-12 21:17:37 +0100

    Updates

---
+++ precache-manifest.e904dbca1344a5ddf61f4cb2884b90d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd613b48d2487fcc5e54faabc0a5b9a4",
+    "url": "/noroff-react-ma3/index.html"
+  },
+  {
+    "revision": "9cf5b623793c6e43653d",
+    "url": "/noroff-react-ma3/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "16a9a9cd7b43e348019e",
+    "url": "/noroff-react-ma3/static/js/2.d227853c.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/noroff-react-ma3/static/js/2.d227853c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf5b623793c6e43653d",
+    "url": "/noroff-react-ma3/static/js/main.ffdfa73c.chunk.js"
+  },
+  {
+    "revision": "add0e389514841524ed6",
+    "url": "/noroff-react-ma3/static/js/runtime-main.7382423a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noroff-react-ma3/precache-manifest.fb29898d76d3a7250a226387559e3ff1.js"
+  "/noroff-react-ma3/precache-manifest.e904dbca1344a5ddf61f4cb2884b90d0.js"
 );
 
 self.addEventListener('message', (event) => {
