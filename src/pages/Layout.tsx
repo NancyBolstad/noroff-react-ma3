95cd6adc663120df21909fbdbdc42358b6d369d4
NancyBolstad <litchacha@gmail.com>
2020-03-12 20:10:58 +0100

    Added header styles

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import Home from './Home';
 import Login from './Login';
 import News from './News';
-import styled from 'styled-components';
+import ThemeWrapper from '../components/ThemeWrapper';
 
 const Header = styled.header`
   width: 100%;
@@ -16,12 +17,20 @@ const Nav = styled.nav`
     justify-content: flex-start;
     align-items: center;
     list-style-type: none;
+    padding-top: 1.5rem;
   }
   a {
-    color: ${props => props.theme.colors.primaryVariant};
+    color: ${props => props.theme.colors.secondaryVariant};
     font-size: 2rem;
     font-weight: bold;
-    margin-right: ${props => props.theme.spacing.m}rem;
+    margin-right: 1.8rem;
+    text-decoration: none;
+    border: 1px solid ${props => props.theme.colors.secondaryVariant};
+    padding: 0.5rem 2.2rem;
+    border-radius: 20px;
+  }
+  a:hover {
+    opacity: 0.8;
   }
 `;
 
@@ -31,21 +40,23 @@ const Layout: React.FunctionComponent<Props> = () => {
   return (
     <Router>
       <div>
-        <Header>
-          <Nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/news">News</Link>
-              </li>
-            </ul>
-          </Nav>
-        </Header>
+        <ThemeWrapper>
+          <Header>
+            <Nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/news">News</Link>
+                </li>
+              </ul>
+            </Nav>
+          </Header>
+        </ThemeWrapper>
 
         {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
