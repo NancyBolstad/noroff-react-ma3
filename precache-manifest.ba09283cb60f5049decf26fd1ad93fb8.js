1666814f9676cc18405c6d81555540e4d24f6bfb
NancyBolstad <litchacha@gmail.com>
2020-03-14 20:18:26 +0100

    Updates

---
+++ precache-manifest.ba09283cb60f5049decf26fd1ad93fb8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d54ca6c32b46650068d2db93a09b003",
+    "url": "/noroff-react-ma3/index.html"
+  },
+  {
+    "revision": "f96751dbf97c96edfd09",
+    "url": "/noroff-react-ma3/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e69d0f32ada9253be5ec",
+    "url": "/noroff-react-ma3/static/js/2.5eeb78bd.chunk.js"
+  },
+  {
+    "revision": "98aafc21bb7fa7185dd093464a540d9d",
+    "url": "/noroff-react-ma3/static/js/2.5eeb78bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5337c13dbe966b772a05",
+    "url": "/noroff-react-ma3/static/js/3.9ef86e43.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/noroff-react-ma3/static/js/3.9ef86e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692205076b5cda218dbd",
+    "url": "/noroff-react-ma3/static/js/4.d3289d3f.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/noroff-react-ma3/static/js/4.d3289d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b7b0f1d69ebc625d30",
+    "url": "/noroff-react-ma3/static/js/5.b0534341.chunk.js"
+  },
+  {
+    "revision": "f06a692e7f4feb2282af",
+    "url": "/noroff-react-ma3/static/js/6.2b5bed56.chunk.js"
+  },
+  {
+    "revision": "9e93b3eacd029673128d",
+    "url": "/noroff-react-ma3/static/js/7.e2c48833.chunk.js"
+  },
+  {
+    "revision": "79f3fbb40721116d6248",
+    "url": "/noroff-react-ma3/static/js/8.806aff9d.chunk.js"
+  },
+  {
+    "revision": "f96751dbf97c96edfd09",
+    "url": "/noroff-react-ma3/static/js/main.4dcd9209.chunk.js"
+  },
+  {
+    "revision": "5bb44d6c41b4ac6c132c",
+    "url": "/noroff-react-ma3/static/js/runtime-main.46ddbb89.js"
+  }
+]);
