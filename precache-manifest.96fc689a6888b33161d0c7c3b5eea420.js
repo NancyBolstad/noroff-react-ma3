1c11ac9c3708d62c516fd20f980e7c8d3785b74c
NancyBolstad <litchacha@gmail.com>
2020-03-14 22:15:03 +0100

    Updates

---
+++ precache-manifest.96fc689a6888b33161d0c7c3b5eea420.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2d25dfd4bcc10071e053bc2506b594",
+    "url": "/noroff-react-ma3/index.html"
+  },
+  {
+    "revision": "1f3c63748764746bd208",
+    "url": "/noroff-react-ma3/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "78097c28b894ce9c38d4",
+    "url": "/noroff-react-ma3/static/js/2.b0665fb3.chunk.js"
+  },
+  {
+    "revision": "98aafc21bb7fa7185dd093464a540d9d",
+    "url": "/noroff-react-ma3/static/js/2.b0665fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a51cec2adee5bea1799",
+    "url": "/noroff-react-ma3/static/js/3.322babdc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/noroff-react-ma3/static/js/3.322babdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c1443887004985127d",
+    "url": "/noroff-react-ma3/static/js/4.b5fcf57e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/noroff-react-ma3/static/js/4.b5fcf57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b24e358879f70aaae69",
+    "url": "/noroff-react-ma3/static/js/5.76a2ddde.chunk.js"
+  },
+  {
+    "revision": "e20596de504d9ff8f49a",
+    "url": "/noroff-react-ma3/static/js/6.c52c10c9.chunk.js"
+  },
+  {
+    "revision": "b9a50fbf14a5402e1003",
+    "url": "/noroff-react-ma3/static/js/7.05a18d74.chunk.js"
+  },
+  {
+    "revision": "6c0325057dd66a79afb4",
+    "url": "/noroff-react-ma3/static/js/8.3c783cc5.chunk.js"
+  },
+  {
+    "revision": "1f3c63748764746bd208",
+    "url": "/noroff-react-ma3/static/js/main.b0b71f61.chunk.js"
+  },
+  {
+    "revision": "cee109f3c44898774ec5",
+    "url": "/noroff-react-ma3/static/js/runtime-main.d66d8ce6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5098657cbf90eb64d7aecf005dc049a.js"
+  "/noroff-react-ma3/precache-manifest.96fc689a6888b33161d0c7c3b5eea420.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noroff-react-ma3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
